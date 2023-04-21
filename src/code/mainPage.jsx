06ab21a8a17a1b6f4f6651b351f672596328b08c
Zaheer Ud Din Akram <zaheerakrambscs@gmail.com>
2023-04-21 23:00:50 +0500

    Reorganized file structure for improved readability and maintainability.
    Fixed file structure location

---
+++ src/code/mainPage.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import "../CSS/mainPage.css"
+function MainPage() {
+  return (
+    <div className='tagOne text-info text-center display-2'>MainPage</div>
+  )
+}
+export default MainPage
