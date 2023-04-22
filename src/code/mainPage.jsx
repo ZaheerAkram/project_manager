c52bb514936e6b2f04b2a1cf14c624dfe8964d40
Zaheer Ud Din Akram <zaheerakrambscs@gmail.com>
2023-04-22 13:46:13 +0500

    Added basic navbar structure and styling.

--- src/code/mainPage.jsx
+++ src/code/mainPage.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
 import "../CSS/mainPage.css"
+import Header from './header'
 function MainPage() {
   return (
-    <div className='tagOne text-info text-center display-2'>MainPage</div>
+    <div className='tagOne text-info text-center'>
+      <Header/>
+    </div>
   )
 }
 export default MainPage
