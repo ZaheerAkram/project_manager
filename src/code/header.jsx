d03d30b4c4aace8467498e377e25d633f4d01602
Zaheer Ud Din Akram <zaheerakrambscs@gmail.com>
2023-04-23 14:15:24 +0500

    Modified navbar styling.

--- src/code/header.jsx
+++ src/code/header.jsx
@@ -3,24 +3,26 @@ import React from 'react'
 function Header() {
   return (
     <div>
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
-            <div class="container-fluid">
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div class="container-fluid row align-items-center">
                 <a class="navbar-brand" href="#">Navbar</a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
                 </button>
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <div class="collapse navbar-collapse d-flex justify-content-center" id="navbarSupportedContent">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0 align-items-center">
                         <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="#">Home</a>
+                            <a class="nav-link active" aria-current="page" href="#">
+                                Home
+                            </a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="#">Link</a>
+                            <a class="nav-link" href="#">About US</a>
                         </li>
                         <li class="nav-item dropdown">
                             <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                             Dropdown
-                        </a>
+                            </a>
                         <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
                             <li><a class="dropdown-item" href="#">Action</a></li>
                             <li><a class="dropdown-item" href="#">Another action</a></li>
@@ -28,12 +30,19 @@ function Header() {
                             <li><a class="dropdown-item" href="#">Something else here</a></li>
                         </ul>
                         </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">
+                                <button class="btn btn-outline-info" type="submit">
+                                    Create Project
+                                </button>
+                            </a>
+                        </li>
                     </ul>
                 </div>
                 <div className='d-flex'>
                     <form class="d-flex justify-content-end">
                         <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-                        <button class="btn btn-outline-success" type="submit">Search</button>
+                        <button class="btn btn-info" type="submit">Search</button>
                     </form>
                 </div>
             </div>
